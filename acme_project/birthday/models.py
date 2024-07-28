33c0dd20799f615c2ae3f4cb65960e19935adf1c
Vladislav Vasilev <vladen21@yandex.ru>
2024-07-28 18:59:21 +0300

    Commit 002

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,21 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
+
+class Birthday(models.Model):
+    first_name = models.CharField(verbose_name='Имя', max_length=20)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=20, blank=True, help_text='Необязательное поле')
+    birthday = models.DateField(verbose_name='Дата рождения')
+
+
+
+class Contest(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=20)
+    description = models.TextField(verbose_name='Описание')
+    price = models.IntegerField(
+        verbose_name='Цена',
+        validators=[MinValueValidator(10), MaxValueValidator(100)],
+        help_text='Рекомендованная розничная цена'
+    )
+
+    comment = models.TextField(verbose_name='Комментарий', blank=True)
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,9 +1,17 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
 
 
-def birthday(request):
-    form = BirthdayForm()
+def birthday(request: HttpRequest):
+    form = BirthdayForm(request.GET or None)
     context = {'form': form}
+    if form.is_valid():
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context=context)
+
